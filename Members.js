be49410072e851d950e3a632a895e0c1f151c55c
Shakeel Hoosain <62466466+ZeroDaySamurai@users.noreply.github.com>
2021-10-22 19:15:50 +0200

    Third Commit

---
+++ Members.js
@@ -0,0 +1,40 @@
+const members =[
+    {
+        id: 1,
+        name: 'Shakeel',
+        email: 'shakeel@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Shanaaz',
+        email: 'naaz@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Dad',
+        email: 'daddy@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 4,
+        name: 'Neezam',
+        email: 'neezam@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 5,
+        name: 'Cornielle',
+        email: 'coras@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 6,
+        name: 'Jada',
+        email: 'Jade@gmail.com',
+        status: 'inactive'
+    }
+]
+
+module.exports = members;
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { request } = require('http');
 const path = require('path');
+const logger = require('./middleware/logger');
 
 const app = express();
 
+//Init middleware
+//app.use(logger);
+
 //Set static folder
-app.listen(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Members API Routes
+app.use('/api/members', require('./routes/api/members'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started in port ${PORT}`));
